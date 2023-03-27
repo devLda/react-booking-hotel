01ddd9ece6b421141293b556de39f31fccbe887c
devLda <leducanh.mywork@gmail.com>
2023-03-28 06:10:50 +0700

    update 28/03

--- src/components/UI/slider/SliderHome.jsx
+++ src/components/UI/slider/SliderHome.jsx
@@ -1,11 +1,99 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
+
 import "../../../styles/slider.css";
 
 const SliderHome = ({ sliders }) => {
   const [indexCurrent, setIndexCurrent] = useState(0);
   const [autoplay, setAutoplay] = useState(true);
+
+  const customSelect = () => {
+    var x, i, j, l, ll, selElmnt, a, b, c;
+    /*look for any elements with the class "custom-select":*/
+    x = document.getElementsByClassName("custom-select");
+    l = x.length;
+    for (i = 0; i < l; i++) {
+      selElmnt = x[i].getElementsByTagName("select")[0];
+      ll = selElmnt.length;
+      /*for each element, create a new DIV that will act as the selected item:*/
+      a = document.createElement("DIV");
+      a.setAttribute("class", "select-selected");
+      a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+      x[i].appendChild(a);
+      /*for each element, create a new DIV that will contain the option list:*/
+      b = document.createElement("DIV");
+      b.setAttribute("class", "select-items select-hide");
+      for (j = 1; j < ll; j++) {
+        /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+          /*when an item is clicked, update the original select box,
+        and the selected item:*/
+          var y, i, k, s, h, sl, yl;
+          s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+          sl = s.length;
+          h = this.parentNode.previousSibling;
+          for (i = 0; i < sl; i++) {
+            if (s.options[i].innerHTML == this.innerHTML) {
+              s.selectedIndex = i;
+              h.innerHTML = this.innerHTML;
+              y = this.parentNode.getElementsByClassName("same-as-selected");
+              yl = y.length;
+              for (k = 0; k < yl; k++) {
+                y[k].removeAttribute("class");
+              }
+              this.setAttribute("class", "same-as-selected");
+              break;
+            }
+          }
+          h.click();
+        });
+        b.appendChild(c);
+      }
+      x[i].appendChild(b);
+      a.addEventListener("click", function (e) {
+        /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+      });
+    }
+    function closeAllSelect(elmnt) {
+      /*a function that will close all select boxes in the document,
+  except the current select box:*/
+      var x,
+        y,
+        i,
+        xl,
+        yl,
+        arrNo = [];
+      x = document.getElementsByClassName("select-items");
+      y = document.getElementsByClassName("select-selected");
+      xl = x.length;
+      yl = y.length;
+      for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+          arrNo.push(i);
+        } else {
+          y[i].classList.remove("select-arrow-active");
+        }
+      }
+      for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+          x[i].classList.add("select-hide");
+        }
+      }
+    }
+    /*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+    document.addEventListener("click", closeAllSelect);
+  };
+
   const timeOut = useRef;
 
   const handleNext = () => {
@@ -133,7 +221,7 @@ const SliderHome = ({ sliders }) => {
                   </div>
                 </div>
               </div>
-              <div className="col2 c3">
+              <div className="col2 c3 custom-select">
                 <label>Adults</label>
                 <div className="select">
                   <select>
@@ -144,7 +232,7 @@ const SliderHome = ({ sliders }) => {
                   </select>
                 </div>
               </div>
-              <div className="col2 c4">
+              <div className="col2 c4 custom-select">
                 <label>Children</label>
                 <div className="select">
                   <select>
@@ -156,14 +244,15 @@ const SliderHome = ({ sliders }) => {
                   </select>
                 </div>
               </div>
-              <div className="col2 c5">
+              <div className="col2 c5 custom-select">
                 <label>Rooms</label>
-                <div className="select">
+                <div>
                   <select>
+                    <option value="1">None</option>
                     <option value="1">1 Room</option>
-                    <option value="2">2 Rooms</option>
-                    <option value="3">3 Rooms</option>
-                    <option value="4">4 Rooms</option>
+                    <option value="2">2 Room</option>
+                    <option value="3">3 Room</option>
+                    <option value="4">4 Room</option>
                   </select>
                 </div>
               </div>
