01a0eeab26c37057038dbcb08f91fa55970f4ebc
devLda <leducanh.mywork@gmail.com>
2023-04-10 00:49:56 +0700

    update 10/04

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer class="relative block bg-stone-800 py-12 z-10 ">
+      <div class="container flex ">
+        <div class="w-6/12 px-5">
+          <div class="footer-column footer-about">
+            <h3 class="footer-title">ANH OCT HOTEL</h3>
+            <ul class="footer-about-text">
+              <li>
+                <b>Address:</b> 3 Cau Giay Street, Lang Thuong Ward, Dong Da
+                District, Hanoi, Vietnam
+              </li>
+              <li>
+                <b>Zalo/Call:</b> +849028888888
+              </li>
+              <li>
+                <b>Email:</b> Sales@anhocthotel.com
+              </li>
+              <li>
+                <ul className="social-media">
+                  <li>
+                    <a href="https://www.facebook.com">
+                      <i class="ri-facebook-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/?lang=vi">
+                      <i class="ri-twitter-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/">
+                      <i class="ri-instagram-fill"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.tripadvisor.com/">
+                      <i class="ri-plane-line"></i>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-3/12 px-5">
+          <div class="footer-column footer-explore clearfix">
+            <h3 class="footer-title">THINGS TO KNOW</h3>
+            <ul class="footer-explore-list list-unstyled">
+              <li>
+                <Link to="/">FAQS</Link>
+              </li>
+              <li>
+                <Link to="/">Warning</Link>
+              </li>
+              <li>
+                <Link to="/">Location</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-3/12 px-5">
+          <div class="footer-column footer-explore clearfix">
+            <h3 class="footer-title">TERM &amp; CONDITION</h3>
+            <ul class="footer-explore-list list-unstyled">
+              <li>
+                <Link to="/">Terms and Conditions</Link>
+              </li>
+              <li>
+                <Link to="/">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link to="/">Payments &amp; method</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-full text-center text-xs text-white">
+        <p>© Copyright 2023 by Anh Oct Hotel</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
-import SliderHome from '../components/UI/slider/SliderHome'
+import React from "react";
+import SliderHome from "../components/UI/slider/SliderHome";
 
+import room2 from "../assets/img/rooms/2.jpg";
+import room8 from "../assets/img/rooms/8.jpg";
 import sliders from "../assets/fake-data/slider.js";
-
+import "../styles/home.css";
 
 const Home = () => {
   return (
     <>
-      <SliderHome sliders={sliders}/>
-    </>
-  )
-}
+      <SliderHome sliders={sliders} />
 
-export default Home
+      <section class="about section-padding">
+        <div class="container flex">
+          <div class="mb-7 px-3 w-6/12">
+            <span>
+              <i class="star-rating"></i>
+              <i class="star-rating"></i>
+              <i class="star-rating"></i>
+              <i class="star-rating"></i>
+              <i class="star-rating"></i>
+            </span>
+            <div class="section-subtitle">The Anh Oct Hotel</div>
+            <div class="section-title">Enjoy a Luxury Experience</div>
+            <p>
+              Welcome to the best five-star deluxe hotel in New York. Hotel
+              elementum sesue the aucan vestibulum aliquam justo in sapien
+              rutrum volutpat. Donec in quis the pellentesque velit. Donec id
+              velit ac arcu posuere blane.
+            </p>
+            <p>
+              Hotel ut nisl quam nestibulum ac quam nec odio elementum sceisue
+              the aucan ligula. Orci varius natoque penatibus et magnis dis
+              parturient monte nascete ridiculus mus nellentesque habitant
+              morbine.
+            </p>
+            <div class="reservations">
+              <div class="icon">
+                <span class="flaticon-call"></span>
+              </div>
+              <div class="text">
+                <p>Reservation</p>
+                <a href="tel:855-100-4444">855 100 4444</a>
+              </div>
+            </div>
+          </div>
+          <div class="px-3 w-3/12">
+            <img src={room8} alt="room 8" class="mt-20 mb-7" />
+          </div>
+          <div class="px-3 w-3/12">
+            <img src={room2} alt="room 2" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
