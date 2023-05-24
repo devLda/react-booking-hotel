36cbe118f7dc0fb69bf12fb71c0ea0e33cfea94c
devLda <leducanh.mywork@gmail.com>
2023-05-24 18:08:12 +0700

    update 24/05

--- src/components/UI/itembooking/ItemBooking.js
+++ src/components/UI/itembooking/ItemBooking.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
 import moment from "moment";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
+
 import { create } from "../../../store/datphong/datphongSlice";
 import path from "../../../utils/path";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const ItemBooking = ({ phong, infoPhong }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -55,8 +70,8 @@ const ItemBooking = ({ phong, infoPhong }) => {
 
         <div className="float-right">
           <div className="bg-yellow-800 text-white inline-block mr-5">
-            <button className="p-3" onClick={handleClick}>
-              Đặt phòng
+            <button className="p-3" onClick={handleOpen}>
+              Xem chi tiết
             </button>
           </div>
           {/* <div className="bg-yellow-800 text-white inline-block ">
@@ -64,6 +79,51 @@ const ItemBooking = ({ phong, infoPhong }) => {
           </div> */}
         </div>
       </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 800,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h3" component="h2">
+          {phong?.LoaiPhong.TenLoaiPhong}
+          </Typography>
+          <AutoplaySlider
+            className="client-wrapper h-full w-full mt-3"
+            play={true}
+            cancelOnInteraction={false}
+            interval={3000}
+          >
+            {phong?.images?.map((item, index) => (
+              <div className="h-full w-full">
+                <img
+                  className="h-full w-full"
+                  key={index}
+                  src={item}
+                  alt={index}
+                />
+              </div>
+            ))}
+          </AutoplaySlider>
+          <Typography id="modal-modal-description" sx={{ mt: 8 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   );
 };
